8e25f7b2e52eb81953fe6b8ca4c05c1d94cafa2e
codeBYdivy <divymits@gmail.com>
2016-05-12 13:16:18 +0530

    Added pages for error and maintenance

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
 $pageVal = ($_GET['page']);
 if($pageVal === 'error') {
-	include_once('error.html')
+	include_once('error.html');
 }
 else if($pageVal === 'maintenance') {
-	include_once('maintenance.html')	
+	include_once('maintenance.html');	
 }
 else if($pageVal == NULL){
 	print("Welcome to my page");
