087620997915e6a0d47cad10da597bd7a57493f0
codeBYdivy <divymits@gmail.com>
2016-05-12 13:07:31 +0530

    AA

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
+var_dump($_GET['page']);
+exit;
 	$s = include_once($_GET['page'] .".html"); 
 	var_dump($s);
-	exit;
-	if(is_empty($s) || $s== null){
+
+	if(is_empty($_GET['page']) || $_GET['page']== null){
 		print("Welcome to my pages");
 	}
 ?>
